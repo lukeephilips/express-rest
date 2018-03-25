b56f3cd64657914fca34152249cd0159be1829d9
Luke <luke.e.philips@gmail.com>
2018-03-25 14:58:20 -0700

    add Book model, GET and POST routes

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+var db = mongoose.connect('mongodb://localhost/books_api');
+var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+.post(function(req, res) {
+  console.log(req);
+  var book = new Book(req.body);
+  console.log(book);
+  book.save();
+  res.send(book);
+})
+.get(function(req, res){
+  var query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre
+  };
+  Book.find(query, function(err, books){
+    if (err){
+      console.log(err);
+    } else {
+      res.json(books);
+    };
+  });
+});
+bookRouter.route('/Book/:id')
+.get(function(req, res){
+  var id = req.params.id;
+  console.log(req.params.id);
+  Book.findById(id, function(err, book){
+    if (err){
+      console.log(err);
+    } else {
+      res.json(book);
+    };
+  });
+});
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res){
   res.send('API running')
 });
